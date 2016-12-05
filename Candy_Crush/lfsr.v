4b938dab96ec1199d7bb5fd0e606f0cddd71c4e6
Danny Pan <Danny@Danny-Macbook.local>
2016-12-04 20:43:31 -0800

    Added all candy crush files

--- Candy_Crush/lfsr.v
+++ Candy_Crush/lfsr.v
@@ -7,25 +7,25 @@ clk             ,  // clock input
 reset              // reset input
 );
 
-	output [7:0] out;
-
-	input [7:0] data;
+	output [12:0] out;
 	input enable, clk, reset;
-	reg [7:0] out;
-	wire        linear_feedback;
+	reg [12:0] out;
+	wire        linear_feedback1, linear_feedback2, linear_feedback3;
 
-	assign linear_feedback = !(out[7] ^ out[3]);
-
+	assign linear_feedback1 = !(out[7] ^ out[3]);
+	assign linear_feedback2 = !(out[4] ^ out[0]);
+	assign linear_feedback3 = !(linear_feedback2 ^ out[2]);
+	
 	always @(posedge clk)
 	begin
 		if (reset)
 		out <= 8'b0 ;
 		else if (enable) 
 		begin
-			out <= {out[6],out[5],
-					out[4],out[3],
-					out[2],out[1],
-					out[0], linear_feedback};
+			out <= {out[10],out[9], out[8],
+				out[7], out[6],
+				out[5], out[4], out[3], out[2],
+				out[0], linear_feedback3, linear_feedback2};
 		end 
 	end
 endmodule // End Of Module counter
